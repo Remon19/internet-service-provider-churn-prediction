a9e35642ec7746d1c95d4f4b56e652456cb688b5
Remon <remontal3at2019@gmail.com>
2025-05-27 21:21:51 +0300

    Add visualization folder and visualization plots

--- EDA.py
+++ EDA.py
@@ -3,18 +3,43 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns 
 from pathlib import Path
+from scipy.stats import probplot
 import os
 
 dataset_path = Path("F:\MyProjects\internet service provider churn prediction\internet_service_churn.csv")
 df = pd.read_csv(dataset_path)
+visualization_path = Path("F:\MyProjects\internet service provider churn prediction/visualizations")
 
-# print(df.head(5))
-# print(df.describe())
-# print(df.info())
+## Overview of dataset
+print(df.head(5))
+print(df.describe())
+print(df.info())
 
+## Quantifying & Visualizing the missing values
+missing_values = pd.DataFrame({"feature":df.columns, "missing_values_ratio":df.isnull().mean()})
+print(missing_values)
+plt.bar(missing_values["feature"], missing_values["missing_values_ratio"])
+plt.ylabel("Percentage")
+plt.xlabel("Feature")
+plt.title("Percentage of Missing Values")
+plt.xticks(rotation=90)
+plt.tight_layout()
+plt.savefig(Path(visualization_path) / "missing_values.png")
+# plt.show()
+
+
+## Quantifying & Visualizing the unique values  
 df.drop(columns="id", inplace=True)
 df_values = pd.DataFrame({"feature":df.columns, "unique_values_count":df.nunique()})
 # print(df_values)
+plt.bar(df_values["feature"], df_values["unique_values_count"])
+plt.ylabel("Number of Unique Categories")
+plt.xlabel("Feature")
+plt.title("Cardinality")
+plt.xticks(rotation=90)
+plt.tight_layout()
+plt.savefig(Path(visualization_path) / "unique_values_count.png")
+# plt.show()
 
 ## Seperating categorical & numerical fetures
 categorical_features = df_values[(df_values['unique_values_count'] > 0) & (df_values['unique_values_count'] <= 10)]['feature'].to_list()
@@ -23,14 +48,23 @@ categorical_features = df_values[(df_values['unique_values_count'] > 0) & (df_va
 numerical_features = df_values['feature'].drop(categorical_features).to_list()
 # print("Numerial Features: ", ", ".join(numerical_features))
 
+## Visualizing Q-Q plot for numerical features to check for its distribution
+for num_feat in numerical_features:
+    plt.figure()
+    probplot(df[num_feat], plot=plt)
+    plt.title(f"Q-Q Plot of {num_feat}")
+    plt.savefig(Path(visualization_path) / f"q-q_plot_feature_{num_feat}.png")
+    # plt.show()
+
 real_numerical_features = list(df.select_dtypes("float64"))
 # print("Real Numerial Features: ", ", ".join(numerical_features))
 
 discrete_numerical_features = [feature for feature in numerical_features if feature in list(df.select_dtypes("int64"))]
 # print("Discrete Numerial Features: ", ", ".join(discrete_numerical_features))
 
-# sns.pairplot(df, vars=numerical_features, diag_kind="hist")
-# plt.title("Pair Plot")
+df[numerical_features].hist(bins=30, figsize=(12, 6))
+plt.tight_layout()
+plt.savefig(Path(visualization_path) / "histogram_plot_num_features.png")
 # plt.show()
 
 ## Imputing Mising Values
